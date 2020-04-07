c81198263aca54b7413bc44ca3e19cf8e0d53158
Hans-Sebastian <hanssebastian.p@gmail.com>
2020-04-07 19:13:40 -0400

    video background

---
+++ app/assets/javascripts/homes.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // - 1.78 is the aspect ratio of the video
+    // - This will work if your video is 1920 x 1080
+    // - To find this value divide the video's native width by the height eg 1920/1080 = 1.78
+    var aspectRatio = 1.78;
+
+    var video = $('#video iframe');
+    var videoHeight = video.outerHeight();
+    var newWidth = videoHeight*aspectRatio;
+    var halfNewWidth = newWidth/2;
+
+    video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
+
+});
